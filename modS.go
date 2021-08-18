e542534a909469877649a362c5cde3a96738d273
ohir <ohir@fairbe.org>
2021-08-18 19:53:07 +0200

    make MIT for public access

---
+++ modS.go
@@ -0,0 +1,13 @@
+package Server
+
+import (
+	proto "example.com/mP"
+)
+
+const me string = "/mS Server"
+
+func Say() string {
+	return me[3:] +
+		" said: tinker here.\n\tThen: " +
+		proto.Say()
+}
